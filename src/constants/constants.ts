6bd5a1ba9a667d0f496bc42845e761e9ee802454
MarianoHernandezEscobal <mh202428@fi365.ort.edu.uy>
2025-11-04 20:32:56 -0300

    cambios

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://backend.inmobiliariacostaazul.com";
 export const INMO_COOR = [-34.634527586625445, -54.161089900973295]
 export const URL_INMO = "https://front.inmobiliariacostaazul.com"
